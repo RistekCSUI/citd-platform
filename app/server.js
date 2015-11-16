3da80873238a3c0b1041a321d4af3c25293b5e96
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-11-16 13:12:40 +0700

    add session + handle login

--- app/server.js
+++ app/server.js
@@ -3,16 +3,20 @@ var bodyParser = require('body-parser');
 var hbs = require('hbs');
 var express = require('express');
 var http = require('http');
+var session = require('express-session');
+// var MongoStore = require('connect-mongo')(session);
 
 var app = express();
 var server = http.Server(app);
 var io = require('socket.io')(server);
 
+var DB_URL = 'mongodb://localhost/citd';
+
 server.listen(3000);
 console.log('server listening at port 3000');
 
 // EXPRESS CONFIGURATION
-//
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
@@ -21,15 +25,56 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/', function (req, res) {
+app.use(session({
+  secret: 'secret',
+  // store: new MongoStore({ url: DB_URL }),
+  resave: false,
+  saveUninitialized: false
+}));
+
+function loggedIn(req, res, next) {
+  if (!req.session.user)
+    return res.redirect('/');
+  next();
+}
+
+function isAdmin(req, res, next) {
+  if (req.session.user.username !== 'admin')
+    return res.redirect('/code');
+  next();
+}
+
+function notLoggedIn(req, res, next) {
+  if (req.session.user)
+    return res.redirect('/code');
+  next();
+}
+
+app.get('/', notLoggedIn, function (req, res) {
   res.render('login.html');
 });
 
-app.get('/code', function (req, res) {
-  res.render('code.html');
+app.post('/', function (req, res) {
+  if (req.body.username == 'admin' && req.body.password == 'admin') {
+    req.session.user = {
+      id: '123456',
+      username: 'admin'
+    };
+    res.redirect('/admin');
+  } else if (req.body.username == 'user1' && req.body.password == 'user1') {
+    req.session.user = {
+      id: '654321',
+      username: 'user1'
+    };
+    res.redirect('/code');
+  }
 });
 
-app.get('/admin', function (req, res) {
+app.get('/code', loggedIn, function (req, res) {
+  res.render('code.html', { user: req.session.user });
+});
+
+app.get('/admin', loggedIn, isAdmin, function (req, res) {
   res.render('admin.html');
 });
 
